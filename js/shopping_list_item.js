cd0580d70e3e7692988d2d51b6febb21b6bc7547
Joshua Vallejo <joshua.a.vallejo@gmail.com>
2017-06-14 21:04:29 -1000

    1st test done

---
+++ js/shopping_list_item.js
@@ -0,0 +1,6 @@
+class ShoppingListItem{
+  constructor(name) {
+    this.name = name
+  }
+}
+module.exports=ShoppingListItem
--- test/shopping_list_test.js
+++ test/shopping_list_test.js
@@ -2,11 +2,12 @@ var chai = require("chai");
 var expect = chai.expect;
 var should = chai.should();
 
-var ShoppingList  = require("../js/shopping_list_item.js");
+var ShoppingListItem  = require("../js/shopping_list_item.js");
 
 describe("Shopping List Class", function(){
   var shoppingListItem = ShoppingListItem;
+
   it('should be a function', function(){
-    expect(shoppingList.name).to.be.a('function');
+    expect(shoppingListItem).to.be.a('function');
   });
 });
