c8b4b377764c60cf5147d47b8b4463f6c1a8883d
Kelsie Cleaver <kelsiecleaver@Kelsies-MacBook-Pro.local>
2017-06-14 22:04:36 -1000

    updated 5

--- js/shopping_list_item.js
+++ js/shopping_list_item.js
@@ -5,13 +5,11 @@ class ShoppingListItem{
     this.description= description;
     this.finished= false;
 }
-<<<<<<< HEAD
+
 checked(){
   this.finished = true;
   return this.finished;
 }
-=======
 
->>>>>>> edfd51dd921099136c6eaca57dd073b897d5cd7c
 }
 module.exports=ShoppingListItem;
