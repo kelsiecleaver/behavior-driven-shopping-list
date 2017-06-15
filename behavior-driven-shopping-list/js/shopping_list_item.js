bf7436e55ec44800f7839240f6e2450d5e055f3b
Kelsie Cleaver <kelsiecleaver@Kelsies-MacBook-Pro.local>
2017-06-14 22:00:53 -1000

    updated

---
+++ behavior-driven-shopping-list/js/shopping_list_item.js
@@ -0,0 +1,10 @@
+class ShoppingListItem{
+
+  constructor(name, description) {
+    this.name = name;
+    this.description= description;
+    this.finished= false;
+}
+
+}
+module.exports=ShoppingListItem;
---
+++ behavior-driven-shopping-list/test/shopping_list_test.js
@@ -0,0 +1,37 @@
+var chai = require("chai");
+var expect = chai.expect;
+var should = chai.should();
+
+var ShoppingListItem  = require("../js/shopping_list_item.js");
+
+describe("Shopping List Class", function(){
+  var shoppingListItem = ShoppingListItem;
+
+  it('should be a function', function(){
+    expect(shoppingListItem).to.be.a('function');
+  });
+
+  it('should have property name', function(){
+  var jerky= new ShoppingListItem("Jerky");
+  expect(jerky.name).to.equal("Jerky");
+  });
+
+  it("should have a description", function(){
+    var jerky= new ShoppingListItem("jerky", "delicious");
+    expect(jerky.description).to.equal("delicious");
+  });
+
+
+  it("should be finished", function(){
+    var jerky= new ShoppingListItem("jerky", "delicious");
+   jerky.finished.should.equal(false);
+  });
+
+  it("should have a method checked()", function(){
+    var jerky= new ShoppingListItem();
+    expect(jerky.checked()).to.equal(true);
+  })
+
+
+
+});
