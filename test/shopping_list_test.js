0d5f1f1a8ccb320c59077a59974a7b6f7b827e66
Kelsie Cleaver <kelsiecleaver@Kelsies-MacBook-Pro.local>
2017-06-14 20:59:01 -1000

    updated list item

--- test/shopping_list_test.js
+++ test/shopping_list_test.js
@@ -2,10 +2,10 @@ var chai = require("chai");
 var expect = chai.expect;
 var should = chai.should();
 
-var ShoppingList  = require("../js/shopping_list.js");
+var ShoppingList  = require("../js/shopping_list_item.js");
 
 describe("Shopping List Class", function(){
-  var shoppingList = ShoppingList;
+  var shoppingListItem = ShoppingListItem;
   it('should be a function', function(){
     expect(shoppingList.name).to.be.a('function');
   });
